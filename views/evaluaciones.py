69da6630dee3d8c25c9a5f7c67fae2b7546f743c
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2025-06-30 09:42:20 -0300

    Update evaluaciones.py

--- views/evaluaciones.py
+++ views/evaluaciones.py
@@ -162,7 +162,7 @@ def mostrar(supabase):
                 "--hover-color": "#b03a3f",
             },
             "nav-link-selected": {
-                "background-color": "#48d1cc",
+                "background-color": "#5EABD6",
                 "color": "#0C0909",
                 "font-weight": "bold",
                 "border-radius": "8px",
@@ -251,7 +251,7 @@ def mostrar(supabase):
             df_visual = df_visual.sort_values(by=["apellido_nombre", "Fecha_formateada"])
             
             # Paginación
-            registros_por_pagina = 8
+            registros_por_pagina = 10
             total_registros = len(df_visual)
             total_paginas = max(1, (total_registros - 1) // registros_por_pagina + 1)
             paginas = list(range(1, total_paginas + 1))
@@ -509,7 +509,7 @@ def mostrar(supabase):
             df_no_anuladas = df_no_anuladas.sort_values(by=["apellido_nombre", "Fecha_formateada"])
             
             # --- Paginación con selectbox (versión segura) ---
-            registros_por_pagina = 8
+            registros_por_pagina = 10
             total_registros = len(df_no_anuladas)
             total_paginas = max(1, (total_registros - 1) // registros_por_pagina + 1)
             paginas = list(range(1, total_paginas + 1))
@@ -612,7 +612,7 @@ def mostrar(supabase):
             df_visual_anuladas = df_visual_anuladas.sort_values(by=["apellido_nombre", "Fecha_formateada"])
         
             # Configurar paginación
-            registros_por_pagina = 8
+            registros_por_pagina = 10
             total_registros = len(df_visual_anuladas)
             total_paginas = max(1, (total_registros - 1) // registros_por_pagina + 1)  # Siempre al menos 1 página
             paginas = list(range(1, total_paginas + 1))
